48c3b6fbc373a985c7908000985523c3788502a0
HC-Strong <stronghannahc@gmail.com>
2019-09-26 09:52:36 -0700

    Added onDatechangeListener since that's apparently how it must be done and got rid of all my flailings

--- app/src/main/java/com/example/periodtracker/controller/MainActivity.kt
+++ app/src/main/java/com/example/periodtracker/controller/MainActivity.kt
@@ -6,50 +6,54 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.CalendarView
+import android.widget.Toast
 import com.example.periodtracker.R
 import com.example.periodtracker.model.Cycle
 import com.example.periodtracker.services.PeriodData
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.time.LocalDate.now
 import java.util.Calendar.DAY_OF_MONTH
 
 class MainActivity : AppCompatActivity() {
+
+    var curSelectedDate : LocalDate = LocalDate.of(now().year, now().month, now().dayOfMonth)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*val calView = mainCalendarView
-        calView.setOnDateChangeListener() {
 
-        }*/
+        val calendarView = findViewById<CalendarView>(R.id.mainCalendarView)
+
+
+        calendarView?.setOnDateChangeListener {_, year,month,day ->
+            val dateAsString : String = "" + (month+1) + "/" + day + "/" + year
+            curSelectedDate = LocalDate.of(year, month, day)
+            Toast.makeText(this,dateAsString, Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 
 
     fun goToTesting(view: View){
+        Log.d("Nothing", "The view is $view")
         val testingIntent = Intent(this, TestingActivity::class.java)
         startActivity(testingIntent)
     }
 
     fun onAddPeriodClick(view: View){
-        //var myPeriod = PeriodInfo(PeriodInfo.Flow.HEAVY)
-        //Log.d("TAG", "Period logged. It is ${myPeriod.flow.toString().toLowerCase()}")
+        Log.d("Nothing", "The view is $view")
+        // When button is clicked, create new cycle starting on currently selected date
+        // the selected date is stored in a
 
-        //Log.d("TAG", mainCalendarView.get(Calendar.DAY_OF_MONTH).toString())
-        Log.d("DATE", "It is ${mainCalendarView.date.to(DAY_OF_MONTH)}.") // This is NOT returning what I'd expect but at least it's not breaking the app like other attempts
-
-        var dateLong = mainCalendarView.date
-        Log.d("DATE", "$dateLong")
-
-        //mainCalendarView.setDate(1569478294690)
-        dateLong = mainCalendarView.date
-        Log.d("DATE", "After Change: $dateLong")
-
+        Toast.makeText(this,curSelectedDate.toString(), Toast.LENGTH_SHORT).show()
 
 
             ///////////////////////////////////////////////////
-        var newCycle = Cycle(LocalDate.of(2019, 9, 24))
+        val newCycle = Cycle(LocalDate.of(2019, 9, 24))
 
         Log.d("TAG", PeriodData.cycleRecords.toString())
         PeriodData.cycleRecords.add(newCycle)
